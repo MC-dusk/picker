eba4885bd7251e46174699100eb1687684191b89
MC-dusk <52311860+MC-dusk@users.noreply.github.com>
2022-08-27 15:37:15 +0800

    rename

---
+++ picker.py
@@ -0,0 +1,8 @@
+# v1.1
+
+from core import Core
+from gui import Gui
+
+inter = Gui()
+picker = Core(inter)
+picker.run()
--- picker_v1.0.py
+++
@@ -1,6 +0,0 @@
-from core import Core
-from gui import Gui
-
-inter = Gui()
-picker = Core(inter)
-picker.run()
